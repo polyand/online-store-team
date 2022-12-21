90007ce278c9849cb282b8703b288c11a9e12053
Aleksandr Yermolaev <sanyagt50@gmail.com>
2022-12-21 23:06:10 +0300

    fix: resolve enters

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { appendPage, router } from 'utils/router';
 import './index.scss';
-
 window.addEventListener('click', router);
 window.addEventListener('popstate', appendPage);
 document.addEventListener('DOMContentLoaded', appendPage);
