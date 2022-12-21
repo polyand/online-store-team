bf641b8a382ab0dd724589b3bbef4ed6080d8007
Andriy Polyakov <polyandit@gmail.com>
2022-12-21 15:03:17 +0200

    feat: add header

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { appendPage, router } from 'utils/router';
+import { header } from 'components/header';
 import './index.scss';
 
 window.addEventListener('click', router);
 window.addEventListener('popstate', appendPage);
 document.addEventListener('DOMContentLoaded', appendPage);
+
+
+const headerElement = document.querySelector('.header');
+if (headerElement === null) {
+  throw new Error();
+}
+if (header) {
+  if (header instanceof Node) {
+    console.log(header);
+    headerElement.innerHTML = '';
+    headerElement.appendChild(header);
+  }
+}
