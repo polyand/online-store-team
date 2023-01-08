8a7601d186f28f88e6f21729f4949cc3e3addf15
Andriy Polyakov <polyandit@gmail.com>
2023-01-08 16:16:10 +0200

    Store_27 - feat: product catalog extension

--- src/pages/home/home-utils/checkUseFilters.ts
+++ src/pages/home/home-utils/checkUseFilters.ts
@@ -1,4 +1,4 @@
-import { getHtmlElement } from 'utils/getHtmlElement';
+import { getInputElement } from 'utils/getInputElement';
 import { filters } from './filtredProducts';
 import { price, stock } from './createRangeFilters';
 
@@ -6,17 +6,8 @@ export function checkUseFilters(): boolean {
   for (const key in filters) {
     if (key === 'stock' || key === 'price') {
       const value = key === 'price' ? price : stock;
-
-      const fromRange = getHtmlElement(document, `#from${key[0].toUpperCase() + key.slice(1)}`) as HTMLInputElement;
-      if (!(fromRange instanceof HTMLInputElement)) {
-        throw new Error('Must be an HTMLInputElement!');
-      }
-
-      const toRange = getHtmlElement(document, `#to${key[0].toUpperCase() + key.slice(1)}`) as HTMLInputElement;
-      if (!(toRange instanceof HTMLInputElement)) {
-        throw new Error('Must be an HTMLInputElement!');
-      }
-
+      const fromRange = getInputElement(document, `#from${key[0].toUpperCase() + key.slice(1)}`);
+      const toRange = getInputElement(document, `#to${key[0].toUpperCase() + key.slice(1)}`);
       if (fromRange.value != `${value[0]}` || toRange.value != `${value[1]}`) {
         return true;
       }
